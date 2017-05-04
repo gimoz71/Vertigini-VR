aeeaea4fd80b59409b603161de50c80065172106
Gianluca Monti <gm3871@gmail.com>
2017-05-04 19:38:49 +0200

    Commit init

---
+++ Assets/Scripts/restartScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartScene : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if(other.name == "restart")
+		{
+			//Application.LoadLevel ("ambient test");
+			SceneManager.LoadScene ("ambient test");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
